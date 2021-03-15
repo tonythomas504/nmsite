fa69fe517aca19f0de5d1c62004c4fb692e52a74
Tony Thomas <tonythomas504@gmail.com>
2021-03-15 16:33:48 -0400

    changing contact uspage to be correct

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -9,9 +9,9 @@ const ContactUs = () => {
 
             <img src={team} alt="" />
             <div className="contact_text">
-                <h1>How To apply</h1>
+                <h1>Contact Us</h1>
                 <div className="contact_paragraph">
-                    <p>The application process opens on March 1st, 2021 and runs through May 1st, 2021. Interested applicants should send resumes to <a href="mailto:lead@nm.com">lead@nm.com</a>and include a 250-word(max) response to the question “Why is this program important to you”.</p>
+                    <p>We look forward to continue providing opportunities to African-American and Black students who attend DePauw University, Marian Univerisity, IUPUI and University of Indianapolis. If you have any questions about our LEAD program please contact us at <a href="mailto:lead@nm.com">lead@nm.com</a> we'd love to hear from you!</p>
                 </div>
             </div>
 
